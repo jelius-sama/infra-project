083c1d76876a2ebbb75cadef76d97642f635c483
Jelius Basumatary <personal@jelius.dev>
2026-08-22 03:16:46 +0530

    docs: improve details in documentation
    Warning this is re-written by AI and may contain errors as well as
    personal touch to the wordings. Will take a better look later, it is
    already 3:18 AM IST.
    
    Signed-off-by: Jelius Basumatary <personal@jelius.dev>

--- src/components/provider.tsx
+++ src/components/provider.tsx
@@ -1,8 +1,14 @@
 "use client";
 import SearchDialog from "@/components/search";
 import { RootProvider } from "fumadocs-ui/provider/next";
-import { type ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, type ReactNode } from "react";
 
 export function Provider({ children }: { children: ReactNode }) {
+  const pathname = usePathname();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return <RootProvider search={{ SearchDialog }}>{children}</RootProvider>;
 }
